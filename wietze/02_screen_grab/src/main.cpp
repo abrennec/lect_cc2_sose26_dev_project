eb32aa49713ef2b3aa7ff16fc60caaff010defaa
Johannes Dietze <Johannes.Dietze@web.de>
2026-06-12 16:52:47 +0200

    fixed mess

---
+++ wietze/02_screen_grab/src/main.cpp
@@ -0,0 +1,28 @@
+#include "ofMain.h"
+#include "ofApp.h"
+
+//========================================================================
+int main( ){
+    
+	// ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
+
+	// // this kicks off the running of my app
+	// // can be OF_WINDOW or OF_FULLSCREEN
+	// // pass in width and height too:
+	// ofRunApp(new ofApp());
+
+    ofGLFWWindowSettings settings;
+    settings.setSize(1024, 768);
+    settings.setGLVersion(4, 1);
+    settings.multiMonitorFullScreen = false;
+    auto window = ofCreateWindow(settings);
+    
+    ofRunApp(window, make_shared<ofApp>());
+    ofRunMainLoop();
+
+
+    private:
+    void drawBalls();
+    void Screenshot();
+    void PixelGrid();
+}
